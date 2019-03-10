88a8e4b8c3e80d2a3aa2eaa87cef39ddf2fab34b
Anik Das <anikdas012@gmail.com>
2019-03-11 03:50:57 +0600

    Added comment

--- app/src/main/java/anikdas/tk/anikdas012/humaclabproject/ui/MapActivity.kt
+++ app/src/main/java/anikdas/tk/anikdas012/humaclabproject/ui/MapActivity.kt
@@ -3,6 +3,11 @@ package anikdas.tk.anikdas012.humaclabproject.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import anikdas.tk.anikdas012.humaclabproject.R
+/**
+ * Created by "Anik Das" on 11-Mar-2019
+ * Developer email: "anikdas012@gmail.com"
+ */
+
 
 class MapActivity : AppCompatActivity() {
 

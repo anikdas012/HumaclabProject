fc90131b00b70fed69c98b2009cf511afac0bbbf
Anik Das <anikdas012@gmail.com>
2019-03-11 02:57:00 +0600

    Removed unnecessary line break and import

--- app/src/main/java/anikdas/tk/anikdas012/humaclabproject/ui/LogInFragment.kt
+++ app/src/main/java/anikdas/tk/anikdas012/humaclabproject/ui/LogInFragment.kt
@@ -9,7 +9,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
-import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.AppCompatTextView
@@ -119,6 +118,4 @@ class LogInFragment : Fragment() {
 
         mRequest.add(request)
     }
-
-
 }

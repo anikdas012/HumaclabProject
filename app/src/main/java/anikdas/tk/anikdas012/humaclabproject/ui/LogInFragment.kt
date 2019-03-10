099707c4e846e3f872ab7648eeb127f591a5ed7a
Anik Das <anikdas012@gmail.com>
2019-03-10 17:47:07 +0600

    Created new fragment for log in

---
+++ app/src/main/java/anikdas/tk/anikdas012/humaclabproject/ui/LogInFragment.kt
@@ -0,0 +1,28 @@
+package anikdas.tk.anikdas012.humaclabproject.ui
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import anikdas.tk.anikdas012.humaclabproject.R
+
+/**
+ * Created by "Anik Das" on 10-Mar-2019
+ * Developer email: "anikdas012@gmail.com"
+ */
+
+class LogInFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_log_in, container, false)
+    }
+
+
+}

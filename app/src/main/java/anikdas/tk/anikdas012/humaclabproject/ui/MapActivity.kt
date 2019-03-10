5d27247b2c1fb6965dd628e2f0eab6c2bc8294d2
Anik Das <anikdas012@gmail.com>
2019-03-11 03:08:46 +0600

    Created new activity to show map

---
+++ app/src/main/java/anikdas/tk/anikdas012/humaclabproject/ui/MapActivity.kt
@@ -0,0 +1,13 @@
+package anikdas.tk.anikdas012.humaclabproject.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import anikdas.tk.anikdas012.humaclabproject.R
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}

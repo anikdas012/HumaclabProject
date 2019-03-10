1952e8379ba5e1112fee10bcdba646f68ca5e8bc
Anik Das <anikdas012@gmail.com>
2019-03-11 03:56:13 +0600

    Initialized google mapView

--- app/src/main/java/anikdas/tk/anikdas012/humaclabproject/ui/MapActivity.kt
+++ app/src/main/java/anikdas/tk/anikdas012/humaclabproject/ui/MapActivity.kt
@@ -3,6 +3,9 @@ package anikdas.tk.anikdas012.humaclabproject.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import anikdas.tk.anikdas012.humaclabproject.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+
 /**
  * Created by "Anik Das" on 11-Mar-2019
  * Developer email: "anikdas012@gmail.com"
@@ -11,8 +14,14 @@ import anikdas.tk.anikdas012.humaclabproject.R
 
 class MapActivity : AppCompatActivity() {
 
+    lateinit var map: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+//        Initialing view components
+        map = findViewById(R.id.map)
+        map.getMapAsync(this)
     }
 }

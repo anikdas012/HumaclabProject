0537d602ab97b9738247dd7d4bfa1424536c9cec
Anik Das <anikdas012@gmail.com>
2019-03-10 18:40:27 +0600

    Added comment

--- app/src/main/java/anikdas/tk/anikdas012/humaclabproject/MainActivity.kt
+++ app/src/main/java/anikdas/tk/anikdas012/humaclabproject/MainActivity.kt
@@ -3,6 +3,11 @@ package anikdas.tk.anikdas012.humaclabproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Created by "Anik Das" on 10-Mar-2019
+ * Developer email: "anikdas012@gmail.com"
+ */
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
